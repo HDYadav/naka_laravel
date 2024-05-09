2f7568a2a826fe40ccebf3eea8fa7a4b0312f196
Hari Yadav <h.hariy2k@gmail.com>
2024-05-09 23:56:47 +0530

    test

--- app/Http/Controllers/Jobs/JobsCotroller.php
+++ app/Http/Controllers/Jobs/JobsCotroller.php
@@ -308,7 +308,7 @@ class JobsCotroller extends ApiController
         $jobs = $jobsQuery->select('j.id','jp.name as jobPosition', 'cl.name as company', 'jc.name as city', 'js.name as state', 
         'et.name as employeementType', 'wp.name as workPlace',
             'j.created_at as date',
-            DB::raw('CASE WHEN j.isFavourite = "true" THEN true ELSE false END as isFavourite'))->get(); 
+            DB::raw('CASE WHEN j.isFavourite = "1" THEN true ELSE false END as isFavourite'))->get(); 
  
 
         return $this->sucessResponse(null, $jobs, true, 201);
@@ -356,7 +356,7 @@ class JobsCotroller extends ApiController
                 'et.name as employmentType',
                 'wp.name as workPlace',
                 'j.created_at as date',
-                DB::raw('CASE WHEN j.isFavourite = "true" THEN true ELSE false END as isFavourite')
+                DB::raw('CASE WHEN j.isFavourite = "1" THEN true ELSE false END as isFavourite')
             )
             ->get();
 
