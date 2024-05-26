e3f056aacc70a752d1a5c5042447ab3d02008a4c
Hari Yadav <h.hariy2k@gmail.com>
2024-05-26 12:12:31 +0530

    changes password

--- app/Http/Requests/PasswordRequest.php
+++ app/Http/Requests/PasswordRequest.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules\Password;
 
 class PasswordRequest extends FormRequest
 {
@@ -22,57 +21,32 @@ class PasswordRequest extends FormRequest
     {
         return [
             'current_password' => [
-                'required', 
+                'required',
                 function ($attribute, $value, $fail) {
-                    if (!Hash::check($value, auth()->user()->password)) {
+                    $user = auth()->user();
+                    if (!$user || !Hash::check($value, $user->password)) {
                         $fail('Old password is incorrect.');
                     }
                 },
             ],
-            'password' => 'required'
+            'password' => 'required', // Additional rules like min length and confirmation
         ];
     }
 
     public function messages()
     {
         return [
-            'password.required' => 'Password is required.',          
+            'password.required' => 'Password is required.',           
+            'current_password.required' => 'Current password is required.',
         ];
     }
 
-    public function failedValidation(Validator $validator)
+    protected function failedValidation(Validator $validator)
     {
         $errors = $validator->errors();
-
-        if ($errors->has('password')) {
-            throw new HttpResponseException(response()->json([
-                'success' => false,
-                'message' => $errors->first('password') 
-            ], 200));
-        }
-
-        if ($errors->has('current_password')) {
-            throw new HttpResponseException(response()->json([
-                'success' => false,
-                'message' => $errors->first('current_password') 
-            ], 200));
-        }
-
-        // If no specific field error found, throw generic error
         throw new HttpResponseException(response()->json([
             'success' => false,
-            'message' => 'Validation failed' 
+            'message' => $errors->first(),
         ], 200));
     }
-
-
-
-    // public function failedValidation(Validator $validator)
-    // {
-    //     throw new HttpResponseException(response()->json([
-    //         'success' => false,
-    //         'message' => $validator->errors()->first(),
-    //         'data' => null
-    //     ], 200));
-    // }
 }
