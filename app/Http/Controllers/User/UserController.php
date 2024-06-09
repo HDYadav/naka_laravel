866891c163eb1b7e521a44c2b4787bb37b01ba22
Hari Yadav <h.hariy2k@gmail.com>
2024-06-09 22:06:39 +0530

    first

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -366,8 +366,8 @@ class UserController extends ApiController
                 ->leftJoin('jobs as j', 'j.id', '=', 'aj.job_id')
                 ->leftJoin('users as u', 'u.id', '=', 'aj.user_id')
                 ->leftJoin('job_positions as jp', 'jp.id', '=', 'j.jobPosiiton')
-                ->where('aj.user_id', $request->user_id)
-                ->get();
+                ->where('aj.user_id', $request->user_id);
+                
                  
             //  return $users;
             
