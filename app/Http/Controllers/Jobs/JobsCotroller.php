5fb8040bdc153705abc077fa806ec49365da57e5
Hari Yadav <h.hariy2k@gmail.com>
2024-06-10 00:17:33 +0530

    first

--- app/Http/Controllers/Jobs/JobsCotroller.php
+++ app/Http/Controllers/Jobs/JobsCotroller.php
@@ -880,7 +880,7 @@ class JobsCotroller extends ApiController
             ->join('users as u', 'u.id', '=', 'aj.user_id')
             ->leftJoin('jobs as j', 'j.id', '=', 'aj.job_id')
             ->leftJoin('job_positions as jp', 'jp.id', '=', 'j.jobPosiiton')
-            ->join('employer_favorates as ef', 'ef.job_id', '=', 'j.id')
+            ->leftJoin('employer_favorates as ef', 'ef.user_id', '=', 'j.created_by')
             ->select('aj.user_id as empoyee_id', 'u.name', 'u.profilePic', 'jp.name as profession', 'ef.isFavourite','j.id as job_id','j.created_by')
               ->where('j.created_by', $user->id) 
                 ->where('aj.isApplyed', 1)          
